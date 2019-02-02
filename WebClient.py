094d2eac4a3e54f04d51303e59a01c4d7e143b78
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2019-02-02 14:08:19 -0600

    Started client side app for testing

---
+++ WebClient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import socket
+
+
+def main():
+    s = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+    # now connect to the web server on port 80
+    # - the normal http port
+    s.connect((socket.gethostname(), 8080))
+
+
+if __name__ == '__main__':
+    main()
