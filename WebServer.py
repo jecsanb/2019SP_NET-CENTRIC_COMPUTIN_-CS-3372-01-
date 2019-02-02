143e624eb083d6bb1ebee4a6679b31b0d1db0c11
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2019-02-02 12:16:51 -0600

    Added to git, start of project

---
+++ WebServer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Jecsan Blanco
+# 2019SP NET-CENTRIC COMPUTIN (CS-3372-01)
+# In this assignment you will develop a simple web server using Python with the following requirements:
+#
+# 1 The port can be defined at the command line, else it defaults to 8080.
+# (ex. server.py 8888)
+#
+# 2 The content for the webserver will be directory where the server is run from.
+#
+# 3 A 404 error message will be displayed for any file that is not found.
+#
+# 4 The program will terminate after responding to the first request
+#
+# Hint: The socket module from python will be very useful.
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == '__main__':
+    main()
