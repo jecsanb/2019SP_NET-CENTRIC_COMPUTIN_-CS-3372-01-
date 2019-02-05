063053191fe8ef15e92ff7efae22d5ebd2462bfb
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2019-02-05 12:26:49 -0600

    simple refactor and put connection waiting in to a  loop and added time and date to reply

--- WebServer.py
+++ WebServer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # Jecsan Blanco
 # 2019SP NET-CENTRIC COMPUTIN (CS-3372-01)
@@ -14,22 +14,23 @@
 # 4 The program will terminate after responding to the first request
 #
 # Hint: The socket module from python will be very useful.
-
+import datetime
 import sys
 import os
 import socket
 
-ROOT_PATH = "./"
-DEFAULT_PORT = "8080"
+ROOT_PATH = './'
+DEFAULT_PORT = '8080'
 
 
 def getfilecontents(file):
-    # print("File requested: " + file)
+    print('File requested: ' + file)
     data = None
     for filename in os.listdir(ROOT_PATH):
+        print(str(filename))
         if os.path.isfile(filename) and file == filename:
-            # print(os.path.join("./", filename))
-            f = open(filename, "+r")
+            print(os.path.join('./', filename))
+            f = open(filename, 'r')
             data = f.read()
             break
     return data
@@ -53,30 +54,35 @@ def main():
     serversocket.bind((socket.gethostname(), int(port)))
     serversocket.listen(5)
 
-    # process the requests when connected
-    print("Listening on port: " + str(port))
+    while 1:
+        # process the requests when connected
+        print('Listening on port: ' + str(port))
 
-    # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
+        # accept connections from outside
+        (clientsocket, address) = serversocket.accept()
 
-    # now do something with the clientsocket
-    print("Connected to client: " + str(address))
+        # now do something with the clientsocket
+        print('Connected to client: ' + str(address))
 
-    data = clientsocket.recv(1024).decode().split(" ")
-    request = data[0]
+        data = clientsocket.recv(1024).decode().split(' ')
+        request = data[0]
 
-    # check for GET, server only accpets GET requests
-    if request != 'GET':
-        clientsocket.send("HTTP/1.1 400 Bad Request\r\n".encode())
-        quit()
-    data = getfilecontents(data[1])
-    if data is None:
-        clientsocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-    else:
-        clientsocket.send(data.encode())
+        # check for GET, server only accpets GET requests
+        if request != 'GET':
+            reply = 'HTTP/1.1 400 Bad Request\r\n'
+        else:
+            data = getfilecontents(data[1])
+            if data is None:
+                reply = 'HTTP/1.1 404 Not Found\r\n'
+            else:
+                reply = 'http/1.1 200 OK\r\n'
+        date = 'Date: ' + str(datetime.datetime.now()) + '\r\n'
+        reply += date + str(data) + "\r\n"
+        print(reply)
+        clientsocket.send(reply.encode())
 
-    clientsocket.close()
-    serversocket.close()
+    # clientsocket.close()
+    # serversocket.close()
 
 
 if __name__ == '__main__':
