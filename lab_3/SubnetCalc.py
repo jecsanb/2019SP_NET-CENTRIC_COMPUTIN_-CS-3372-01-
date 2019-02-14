a70fa267d52168b12e0b84ee852eec825d1a8de5
Jecsan Blanco <jecsanb@outlook.com>
2019-02-14 11:40:56 -0600

    added ipv4 address tester

--- lab_3/SubnetCalc.py
+++ lab_3/SubnetCalc.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+def isvalidipv4(address):
+    valid = False
+    candidate = str(address).split('.')
+    if len(candidate) == 4:
+        for octet in candidate:
+            valid = True if (octet.isalnum() and 0 <= int(octet) <= 255) else False
+            if not valid:
+                break
+    return valid
+
+
 def main():
-    print("Hello world!")
+    ipv4 = str(input('Enter ipv4 address: ')).strip()
+    print(isvalidipv4(ipv4))
 
 
 if __name__ == '__main__':
