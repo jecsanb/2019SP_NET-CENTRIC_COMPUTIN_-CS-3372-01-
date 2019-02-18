483bf0a3923930d5918ebc0c4728d6159da1eff3
Jecsan Blanco <jecsanb@outlook.com>
2019-02-17 21:46:24 -0600

    added ipv4 address tester

--- lab_3/SubnetCalc.py
+++ lab_3/SubnetCalc.py
@@ -10,9 +10,29 @@ def isvalidipv4(address):
     return valid
 
 
+def isvalidmask(mask):
+    return True if 1 <= int(mask) <= 32 else False
+
 def main():
-    ipv4 = str(input('Enter ipv4 address: ')).strip()
-    
+    ipv4_octets = []
+    while True:
+        # read needed input
+        ipv4 = input('Enter ipv4 address: ').strip()
+        mask = input('Enter mask bits: ').strip()
+        if isvalidipv4(ipv4) and isvalidmask(mask):
+            ipv4_octets = ipv4.split('.')
+            break
+        else:
+            print("Invalid address or mask")
+
+    # a. Subnet or Network ID (first IP address in the network range)
+    print("Network Subnet: ")
+    # b. Broadcast Address (last IP in the network range)
+    print("Broadcast Address:")
+    # c. Subnet Mask in decimal form (i.e. 255.255.255.192)
+    print("Subnet Mask:")
+    # d. Maximum number of hosts allowed in the network
+    print("Hosts:")
 
 
 if __name__ == '__main__':
