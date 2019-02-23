a4ec1b030b2e10e359a86bb7407d301c69aa5862
Jecsan Blanco <jecsanb@outlook.com>
2019-02-23 13:57:20 -0600

    Minor cleanup and refactor

--- lab_3/SubnetCalc.py
+++ lab_3/SubnetCalc.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 __author__ = 'Jecsan Blanco'
 
+# A basic ipv4 subnet calculator
+
 
 def is_valid_ip4(address):
-    '''Check if address is a valid ipv4 address'''
+    # Check if address is a valid ipv4 address
     valid = False
     candidate = str(address).split('.')
     if len(candidate) == 4:
@@ -19,24 +21,26 @@ def is_valid_mask(mask):
 
 
 def bin_to_ip4(binary):
-    '''Converts a 32 bit number in to an ipv4 formatted string'''
+    # Converts a 32 bit number in to an ipv4 formatted string
     mask = 255
     octets = []
+    # turn each octet in to a decimal format
     for i in range(0, 4):
         octets.append(str(binary & mask))
         binary = binary >> 8
+    # add the . separators
     return '.'.join(reversed(octets))
 
 
 def ip4_to_bin(address):
-    # 192.168.1.1
-    ''' Converts a ipv4 formatted address string in to a 32 bit binary number '''
+    #  Converts a ipv4 formatted address string in to a 32 bit binary number
     if not is_valid_ip4(address):
         print('Error, invalid ip')
         return None
-    # break in to 4 decimals and convert to 8 bit binary
+    # break in to 4 decimals
     octets = address.split('.')
     number = 0
+    # create one binary string
     for octet in octets:
         number = (number << 8) | int(octet)
     return number
@@ -52,19 +56,19 @@ def main():
         else:
             print('Invalid address or mask')
     mask = int(mask)
-    subnetmask = -1 << (32 - mask)
-    network_mask = ~subnetmask
-    binip = ip4_to_bin(ipv4)
-    networksubnet = subnetmask & binip
+    subnet_mask = -1 << (32 - mask)
+    network_mask = ~subnet_mask
+    bin_ip = ip4_to_bin(ipv4)
+    subnet_id = subnet_mask & bin_ip
 
     # a. Subnet or Network ID (first IP address in the network range)
-    print('Network Subnet: %s ' % bin_to_ip4(networksubnet))
+    print('Subnet ID: %s ' % bin_to_ip4(subnet_id))
     # b. Broadcast Address (last IP in the network range)
-    print('Broadcast Address: %s' % bin_to_ip4(networksubnet | network_mask))
+    print('Broadcast Address: %s' % bin_to_ip4(subnet_id | network_mask))
     # c. Subnet Mask in decimal form (i.e. 255.255.255.192)
-    print('Subnet Mask: %s' % bin_to_ip4(subnetmask))
+    print('Subnet Mask: %s' % bin_to_ip4(subnet_mask))
     # d. Maximum number of hosts allowed in the network
-    print('Hosts: %s' % str(network_mask-2))
+    print('Hosts: %s' % str(network_mask - 1))
 
 
 if __name__ == '__main__':
