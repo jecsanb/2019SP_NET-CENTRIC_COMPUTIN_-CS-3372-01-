15b2939cb0aa663cbde77b72d617a4fa32f32646
Jecsan Blanco <jecsanb@outlook.com>
2019-04-05 22:42:57 -0500

    built reply package

--- lab_4/pcapreading.py
+++ lab_4/pcapreading.py
@@ -6,15 +6,17 @@ def main():
     pcap_filename = None
     if len(sys.argv) >= 2:
         # The program will accept the filename of a pcap file on the command line. This can be accomplished with argv
+        # assumes a valid packet name will be given...
         pcap_filename = (str(sys.argv[1])).strip()
     else:
         pcap_filename = 'Assignment5.pcapng'
         # exit()
+
     # 2 The program will read in the pcap file. This can be done with rdpcap in the scapy module.
     packets = rdpcap(pcap_filename)
     packet = packets[0]
-    print(packet.summary())
-    print(packet.show())
+    # print(packet.summary())
+    # print(packet.show())
     #  The program will display the following information from the 1st packet of the pcap file.
     # a. Source IP address
     print("Source IP:   \t%s" % packet[ARP].psrc)
@@ -24,6 +26,14 @@ def main():
     print("Source MAC:  \t%s" % packet.src)
     # d. Destination hardware address
     print("Dest  MAC:   \t%s" % packet.dst)
+
+    # build reply packet
+    reply = Ether(src='00:0c:29:c5:33:72', dst=packet.src, type=2054) \
+            / ARP(hwdst=packet.src, ptype=2048, hwtype=1, psrc=packet[ARP].pdst, hwlen=6, plen=4,
+                  pdst=packet[ARP].psrc, hwsrc='00:0c:29:c5:33:72', op=2)
+
+    # print(reply.summary())
+    # print(reply.show())
 
 
 if __name__ == '__main__':
