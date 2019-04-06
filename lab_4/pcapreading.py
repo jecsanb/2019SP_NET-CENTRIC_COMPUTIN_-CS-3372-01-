33714acc085de8d13ba4dcc3fe4e22c975929b8b
Jecsan Blanco <jecsanb@outlook.com>
2019-04-05 21:54:33 -0500

    success read of pcap file

--- lab_4/pcapreading.py
+++ lab_4/pcapreading.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python2.7
+from scapy.all import *
 
 def main():
-    print("Hello world")
-
+    pcap_filename = None
+    if len(sys.argv) >= 2:
+        pcap_filename = (str(sys.argv[1])).strip()
+    else:
+        pcap_filename = 'Assignment5.pcapng'
+        #exit()
+    packets = rdpcap(pcap_filename)
+    packet = packets[0]
+    print(packet.summary())
 
 if __name__ == '__main__':
     main()
+
